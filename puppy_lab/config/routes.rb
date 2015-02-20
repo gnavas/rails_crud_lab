bdb376b4b3c3fa3ad06af4bd8ac142cbe86de6ee
George Navas <gannavas@gmail.com>
2015-02-19 19:09:41 -0800

    HW submission

--- puppy_lab/config/routes.rb
+++ puppy_lab/config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
-  get 'puppies/index'
+  root 'puppies#index'
 
-  get 'puppies/show'
+  get '/puppies', to: 'puppies#index'
 
-  get 'puppies/edit'
+  get '/puppies/new', to: 'puppies#new'
 
-  get 'puppies/new'
+  get '/puppies/:id', to: 'puppies#show'
+
+  get '/puppies/:id/edit', to: 'puppies#edit'
+
+  post '/puppies', to: 'puppies#create'
+
+  put '/puppies/:id', to: 'puppies#update'
+
+  delete '/puppies/:id', to: 'puppies#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
